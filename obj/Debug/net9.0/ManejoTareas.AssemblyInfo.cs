421fb075fd04947bb6d95a5a76d76aa356c2f443
Diego <diegoargotepabon@gmail.com>
2026-09-16 20:35:43 -0500

    fix

--- obj/Debug/net9.0/ManejoTareas.AssemblyInfo.cs
+++ obj/Debug/net9.0/ManejoTareas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManejoTareas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f117c6401aeb8a2c3c5c0046d4a1fb452e9d7f08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8e82e048a2cf7bd839a86e59c10d2e2f7494efd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManejoTareas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManejoTareas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
